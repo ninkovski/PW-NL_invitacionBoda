d903c4398e165d888ff6a84921017642ed6e48f6
nmoralesj <nmoralesj@pandero.com.pe>
2023-07-10 17:06:56 -0500

    footer header redes

---
+++ swc/modules/components.mjs
@@ -0,0 +1,14 @@
+/*
+	Componentes Generales  
+	StartWebPerú | @ninkovski
+*/
+import { footerContent, headerContent } from '../params/components.js';
+
+window.addEventListener('DOMContentLoaded', function () {
+	var footerContainer = document.getElementById('footer-container');
+	footerContainer.innerHTML = footerContent;
+});
+window.addEventListener('DOMContentLoaded', function () {
+	var footerContainer = document.getElementById('header-container');
+	footerContainer.innerHTML = headerContent;
+});
---
+++ swc/params/components.js
@@ -0,0 +1,54 @@
+/*
+    Variables de Footer  
+    StartWebPerú | @ninkovski
+*/
+
+export const redes = {
+    linkedin:
+        '<li><a ' +
+        'href="https://www.linkedin.com/company/start-web-consulting-per%C3%BA/" ' +
+        'target="_blank" class="fab fa-linkedin"><span class="label">LinkedIn</span></a></li>',
+    facebook:
+        '<li><a ' +
+        'href="https://www.facebook.com/profile.php?id=100094164543754"' +
+        'target="_blank" class="fab brands fa-facebook-f"><span class="label">Facebook</span></a></li>',
+    instagram:
+        '<li><a ' +
+        'href="https://www.instagram.com/start.web.peru/" ' +
+        'target="_blank"  class="fab brands fa-instagram"><span class="label">Instagram</span></a></li>',
+    whatsApp:
+        '<li><a ' +
+        'href="https://api.whatsapp.com/send?phone=51901976987&text=Hola%20me%20encantó%20tu%20pagina%20web%21" ' +
+        'target="_blank"  class="fab brands fa-whatsapp"><span class="label">Email</span></a></li>',
+};
+
+
+export const assets = {
+    class: '<ul class="icons">',
+    close: `</ul></p></div></div>`,
+}
+export const redesGroup = `
+    ${redes.linkedin}
+    ${redes.facebook}
+    ${redes.instagram}
+    ${redes.whatsApp} `;
+
+export const copyright =
+    `<div class="row copyright"><div class="col-md-12 text-center">
+    <p><Strong><small class="block">Powered By </small></Strong>
+    <small class="block">@StarWebConsulting - Perú 2023</a></small></p>
+    <ul class="fh5co-social-icons">
+`;
+
+export const footerContent = `
+    ${copyright}
+    ${redesGroup}
+    ${assets.close}
+`;
+export const headerContent =
+    `<ul>
+        <li class="active"><a href="index.html">Home</a></li>
+        <li><a href="about.html">Story</a></li>
+        <li><a href="gallery.html">Gallery</a></li>
+        <li><a href="contact.html">Contact</a></li>
+    </ul>`;
