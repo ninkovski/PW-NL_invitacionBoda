cd41c035be49dd0927a314a3e0009df141c037f8
nmoralesj <nmoralesj@pandero.com.pe>
2023-07-11 00:05:09 -0500

    nuevos cambios

--- swc/config.js
+++ swc/config.js
@@ -4,7 +4,7 @@
 */
 export const appParams = {
 	Name: "SWC-P WeddingCard_Laura&Ninko",
-	SheetURL: "https://script.google.com/macros/s/AKfycbyMZ_T4G5XyWgXb2mvoPpZ_nqp-zwn_AoinDTO1FHtCrGkEDF3Rp4dj7ELhOHvBtsCx/exec"
+	SheetURL: "https://script.google.com/macros/s/AKfycbwSDt8wFd2coAeefFvTIyWaoXT3frh5Ldyzp4wnxWIhyGSWuDzH1IAEdKLgTICbCwLGeQ/exec"
 };
 export const emailParams = {
 	SecureToken: "60a104f3-47a1-4f81-82f4-abc5a77e14ba",
