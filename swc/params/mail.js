8254bf76208b4d9021c30f168ca5d99de4572b35
nmoralesj <nmoralesj@pandero.com.pe>
2023-07-12 16:18:42 -0500

    version final nuevos fondos

--- swc/params/mail.js
+++ swc/params/mail.js
@@ -6,10 +6,10 @@ export const emailWelcome = {
     Subject: "¡Gracias por Confirmar tu asistencia!",
     Body: `
  <body>
-    <div class="container">
-
-        <p>Contenido por desarrollar.</p>
-       
+    <div class="container"> 
+        <img src="https://ninkovski.github.io/PW-NL_invitacionBoda/images/confirmacion.jpg" alt="Logo" class="logo">
+ 
+        <p class="footer">Este es un mensaje automático, por favor no respondas a este correo.</p>
     </div>
 </body>
     `
