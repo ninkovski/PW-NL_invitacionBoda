f543121534c8a9ebc17febbe479a3224e49ca448
nmoralesj <nmoralesj@pandero.com.pe>
2023-07-17 19:06:27 -0500

    mabel y victo

--- swc/params/invitados.js
+++ swc/params/invitados.js
@@ -71,7 +71,7 @@ export const invitados = [
     [70, "06", "04", "Ninko Familia 6", "Patrick", "Oliveros", "110604"],
     [71, "10", "12", "Laura Amigos", "Luis Entique", "Mora", "111012"],
     [72, "10", "13", "Laura Amigos", "Nadia", "Boza", "111013"],
-    [71, "10", "14", "Laura Amigos", "Ruth Mabel", "Calderon Prado", "111014"],
-    [72, "10", "15", "Laura Amigos", "Victor Angel", "Cardenas Huaytaya", "111015"]
+    [73, "10", "14", "Laura Amigos", "Ruth Mabel", "Calderon Prado", "111014"],
+    [74, "10", "15", "Laura Amigos", "Victor Angel", "Cardenas Huaytaya", "111015"]
 ];
 
